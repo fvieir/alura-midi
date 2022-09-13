e29f6e9ae70ba4b15431616eebc3dac206ef3298
Fabricio Dourado <fcvieira2@gmail.com>
2022-09-12 22:08:13 -0300

    Adicionando eventos de tecaldo

--- js/main.js
+++ js/main.js
@@ -9,9 +9,7 @@ function tecla_ativa (tecla)
 
 function remove_tecla_ativa (tecla)
 {
-  setInterval(() => {
-    tecla.classList.remove('ativa')
-  }, 500)
+  tecla.classList.remove('ativa')
 }
 
 const listaTeclas = document.querySelectorAll('.tecla')
@@ -23,12 +21,20 @@ for (let contador = 0; contador < listaTeclas.length; contador++) {
   
   const idAudio = `#som_${instrumento}`
 
-  teclas.addEventListener('click', () => {
+  teclas.onclick =  function (event) {
     tocar_som(idAudio)
-    tecla_ativa(teclas)
-    remove_tecla_ativa(teclas)
+  }
+  
+  teclas.addEventListener('keydown', (event) => {
+    if (event.code === 'enter' || event.code === 'space') {
+      tecla_ativa(teclas)
+      console.log(event)
+    }
+  })
+
+  teclas.addEventListener('keyup', (event) => {
+    if (event.code === 'enter' || event.code === 'space') {
+      remove_tecla_ativa(teclas)
+    }
   })
 }
-
-
-
